03164dfeb5ac73fabf54c94cf4ada718ab171326
conradoovictor <conrado.victor01@gmail.com>
2025-10-29 17:01:52 -0300

    Instanciação da base de dados

--- src/main/java/com/conradoovictor/workshopmongo/config/Instantiation.java
+++ src/main/java/com/conradoovictor/workshopmongo/config/Instantiation.java
@@ -1,5 +1,31 @@
 package com.conradoovictor.workshopmongo.config;
 
-public class Intatiation {
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.conradoovictor.workshopmongo.domain.User;
+import com.conradoovictor.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
 
 }
