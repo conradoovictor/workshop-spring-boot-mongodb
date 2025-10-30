8f8d52d401fd03910107ee13116018e9159d4f00
conradoovictor <conrado.victor01@gmail.com>
2025-10-30 14:17:24 -0300

    Inserção de usuário com POST

--- src/main/java/com/conradoovictor/workshopmongo/services/UserService.java
+++ src/main/java/com/conradoovictor/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.conradoovictor.workshopmongo.domain.User;
+import com.conradoovictor.workshopmongo.dto.UserDTO;
 import com.conradoovictor.workshopmongo.repository.UserRepository;
 import com.conradoovictor.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
             return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
         }
-}
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+    }
